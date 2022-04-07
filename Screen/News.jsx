fabffdb1f8b55644c55807d1e2deb799dff1f322
nickattack97 <nickattack97@gmail.com>
2022-04-07 19:48:06 +0200

    initial commit

---
+++ Screen/News.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, StyleSheet, Text, Button} from 'react-native'
+
+const News = ({navigation}) => {
+    return(
+        <Text>News Screen</Text>
+    )
+}
+
+export default News
